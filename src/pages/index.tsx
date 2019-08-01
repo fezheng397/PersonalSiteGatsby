5501ac2a02912fdaf54fde56208404f4585f08f1
fezheng397 <fezheng397@gmail.com>
2019-08-01 00:34:04 -0400

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { graphql } from 'gatsby';
-import * as React from 'react';
-import * as styles from './Index.module.scss';
+import { graphql } from "gatsby";
+import * as React from "react";
+import * as styles from "./Index.module.scss";
+import "../styles/core.scss";
 
 interface IndexPageProps {
   data: {
@@ -8,8 +9,8 @@ interface IndexPageProps {
       siteMetadata: {
         name: string;
         tagline: string;
-      },
-    },
+      };
+    };
   };
 }
 
@@ -25,12 +26,8 @@ export const indexPageQuery = graphql`
 `;
 
 export default class IndexPage extends React.Component<IndexPageProps, {}> {
-
   public render() {
-    const {
-      name,
-      tagline,
-    } = this.props.data.site.siteMetadata;
+    const { name, tagline } = this.props.data.site.siteMetadata;
 
     return (
       <div className={styles.Container}>
