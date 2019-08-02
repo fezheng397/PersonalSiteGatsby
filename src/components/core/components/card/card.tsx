762eea07b2b0e59928641378d04c1c73fb634a30
fezheng397 <fezheng397@gmail.com>
2019-08-02 17:53:02 -0400

    setup components

---
+++ src/components/core/components/card/card.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface ICardProps {}
+
+export interface ICardState {}
+
+export class Card extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>Card</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/core/components/navbar/navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface INavbarProps {}
+
+export interface INavbarState {}
+
+export class Navbar extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>Navbar</h1>
+      </div>
+    );
+  }
+}
