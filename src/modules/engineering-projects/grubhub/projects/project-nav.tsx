16decf9ec178b3dcb8afc05bb92730a5f56a1add
fezheng397 <fezheng397@gmail.com>
2019-09-02 01:00:56 -0400

    fixed projectnav height

--- src/modules/engineering-projects/grubhub/projects/project-nav.tsx
+++ src/modules/engineering-projects/grubhub/projects/project-nav.tsx
@@ -49,28 +49,12 @@ const NavItems = styled.ul`
 
 const NavItem = styled.li``;
 
-const shake = () => keyframes`
-  10%, 90% {
-    transform: translate3d(-1px, 0, 0);
-  }
-
-  20%, 80% {
-    transform: translate3d(2px, 0, 0);
-  }
-
-  30%, 50%, 70% {
-    transform: translate3d(-4px, 0, 0);
-  }
-
-  40%, 60% {
-    transform: translate3d(4px, 0, 0);
-  }
-`;
-
 const NavButton = styled(Button)`
+  height: 124px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   padding: ${Styles.Spaces.spacing2} ${Styles.Spaces.spacing16};
   color: ${Styles.Colors.textLightBlack};
 
